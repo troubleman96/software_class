e4d6d9dc08f47f9309074eab10457d74e4f74bc7
troubleman <itslugenge96@gmail.com>
2025-12-04 15:20:24 +0300

    return value

--- functions.py
+++ functions.py
@@ -3,19 +3,19 @@
 
 # greeeting()
 
-def sum ():
-    a = 10
-    b = 20
-    c = 30
-    total = a + b + c
-    print (f"the total is, {total} and values are {a}, {b} and {c}")
+# def sum ():
+#     a = 10
+#     b = 20
+#     c = 30
+#     total = a + b + c
+#     print (f"the total is, {total} and values are {a}, {b} and {c}")
 
-sum()
+# sum()
 
 #using arguments
 def total (x,y,z):
     total = x + y + z
-    print (f"the total is, {total} and values are {x}, {y} and {z}")
+    return (f"the total is, {total} and values are {x}, {y} and {z}")
 
 
-total(2,6,4)    
+print(total(2,6,4))     
