37b5614fd67088ff0aa3753ad2d521dc16cb6f42
troubleman <itslugenge96@gmail.com>
2025-12-04 15:25:35 +0300

    taking input from user

--- functions.py
+++ functions.py
@@ -13,9 +13,20 @@
 # sum()
 
 #using arguments
+# def total (x,y,z):
+#     total = x + y + z
+#     return (f"the total is, {total} and values are {x}, {y} and {z}")
+
+
+# print(total(2,6,4))     
+
+x = int(input("enter ur 1st: "))
+y= int(input("enter ur 2nd: "))
+z= int(input("enter ur 3rd: "))
+
 def total (x,y,z):
     total = x + y + z
     return (f"the total is, {total} and values are {x}, {y} and {z}")
 
 
-print(total(2,6,4))     
+print(total(x,y,z))
