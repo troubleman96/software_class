488231f7aa2c68f7c7770e06d369475aa57b0b0d
troubleman <itslugenge96@gmail.com>
2025-12-04 15:13:21 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+# def greeeting():
+#     print("hello hey")
+
+# greeeting()
+
+def sum ():
+    a = 10
+    b = 20
+    c = 30
+    total = a + b + c
+    print (f"the total is, {total} and values are {a}, {b} and {c}")
+
+sum()
---
+++ loops.py
@@ -0,0 +1,37 @@
+# numbers = [1,2,3,4,5,6,7]
+
+# for number in numbers:
+#     print(number)
+
+
+
+# fruits = ["mango", "oranges", "maparachichi"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+
+# fruit = input("enter some fruits.....")
+# fruits = []
+# fruits.append(fruit)
+
+# fruits = []
+
+# for fruit in range(5):
+#     fruit = input("enter some fruits.....")
+#     fruits.append(fruit)
+
+
+
+# print(fruits)
+
+
+
+
+login = True
+
+while login:
+    i = 1
+    print(i)
+    i=+1
